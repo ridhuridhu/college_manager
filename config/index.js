8a0b963e7b45fca46846c96827b6e40278401329
ridhuridhu <ridhuridhu05@gmail.com>
2020-11-02 18:50:59 +0530

    db updated

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 
 module.exports = {
-  MONGO_URL: process.env.DB_URL
+  MONGO_URL:"mongodb://127.0.0.1/college_manager"
 };
--- libs/db-connection.js
+++ libs/db-connection.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
-const {MONGO_URL} = require('../config/');
+const {
+  MONGO_URL
+} = require('../config/');
 mongoose.Promise = global.Promise;
 
-mongoose.connect(MONGO_URL, {useMongoClient: true});
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true
+});
 
 mongoose.connection
   .once('open', () => console.log('Database is in relationship with Server <3'))
